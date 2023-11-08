7e2dad2e642674773ba807eaeb25426be858f5d4
jflaga <flaga.jeremiah@gmail.com>
2023-11-09 02:06:51 +0800

    Revert "fix (AuthServer): add "ProductCatalog" in scopes of OpenIddict Applications for Admin Panel"
    This reverts commit ca5618dd616ef222c859518e9406909f0f9e1bf5.

--- apps/auth-server/src/ShopNServe.AuthServer.Domain/OpenIddict/OpenIddictDataSeedContributor.cs
+++ apps/auth-server/src/ShopNServe.AuthServer.Domain/OpenIddict/OpenIddictDataSeedContributor.cs
@@ -226,7 +226,7 @@ public class OpenIddictDataSeedContributor : IDataSeedContributor, ITransientDep
                 {
                     OpenIddictConstants.GrantTypes.AuthorizationCode, OpenIddictConstants.GrantTypes.Implicit
                 },
-                scopes: new List<string>(commonScopes) { "AdminPanel", "ProductCatalog" },
+                scopes: new List<string>(commonScopes) { "AdminPanel" },
                 redirectUri: $"{webClientRootUrl}signin-oidc",
                 clientUri: webClientRootUrl,
                 postLogoutRedirectUri: $"{webClientRootUrl}signout-callback-oidc"
